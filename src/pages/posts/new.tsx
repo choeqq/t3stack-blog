e56e6c995472f34aaf200a6b2fe9059cd65d126e
choeqq <k.chola@op.pl>
2022-07-01 18:20:10 +0200

    create post done

---
+++ src/pages/posts/new.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { CreatePostInput } from "../../schema/post.schema";
+import { trpc } from "../../utils/trpc";
+
+export default function CreatePostPage() {
+  const { handleSubmit, register } = useForm<CreatePostInput>();
+  const router = useRouter();
+
+  const { mutate, error } = trpc.useMutation(["posts.create-post"], {
+    onSuccess({ id }) {
+      router.push(`/posts/${id}`);
+    },
+  });
+
+  function onSubmit(values: CreatePostInput) {
+    mutate(values);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {error && error.message}
+
+      <h1>Create posts</h1>
+
+      <input type="text" placeholder="Your post title" {...register("title")} />
+      <br />
+      <textarea placeholder="Your post body" {...register("body")}></textarea>
+      <br />
+
+      <button>Create post</button>
+    </form>
+  );
+}
